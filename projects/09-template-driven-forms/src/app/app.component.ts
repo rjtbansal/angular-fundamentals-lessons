6494a37f1df274d27faa82211e7a94bd6660979e
Rajat Bansal <rjtbansal@gmail.com>
2025-09-28 19:56:03 -0400

    Added a template form example

--- projects/09-template-driven-forms/src/app/app.component.ts
+++ projects/09-template-driven-forms/src/app/app.component.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FormsModule],
+  // template-driven form. No explicit form model in the component class
+  // Angular infers the form model from the template directives
+  // template driven forms are simpler to set up for simple forms or one time forms such as a login form
+  // but less scalable for complex forms compared to reactive forms
   template: `
     <article>
       <h1>Blog Post</h1>
       <section>
         <label for="title">Post Title</label>
-        <!-- add the input -->
-
+         <!-- ngModel creates binding between title and input -->
+          <!-- 
+         ngModel vs React's onChange
+            ngModel in Angular
+              1. Two-way binding: [(ngModel)]="title" handles both reading and writing
+              2. Automatic: You don't write event handlers - Angular manages the synchronization
+              3. Declarative: You just declare what property to bind to
+            onChange in React
+              1. One-way binding: Only handles the "writing" part (user input → state)
+              2. Manual: You must write event handlers to update state
+              3. Imperative: You explicitly handle the state updates
+          -->
+         <input type="text" id="title" [(ngModel)]="title" />
         <label for="body">Post Body</label>
-        <!-- add the textarea -->
+        <textarea id="body" [(ngModel)]="body"></textarea>
       </section>
-      <!-- <section>
-        <p>Display title</p>
-        <p>Display value</p>
-      </section> -->
+      <section>
+        <p>{{ title }}</p>
+        <p>{{ body }}</p>
+      </section>
     </article>
   `,
 })
 export class AppComponent {
-  title = '09-template-driven-forms';
+  title = '';
+  body = '';
 }
