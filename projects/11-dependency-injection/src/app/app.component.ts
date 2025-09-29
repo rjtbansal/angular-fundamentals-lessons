a0dc88ffac0dc286e0ca89325f6b4617e1d65dc1
Rajat Bansal <rjtbansal@gmail.com>
2025-09-28 22:13:05 -0400

    Dependency injection example

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: ` 
+  <h1>User Listing</h1>
+  @for(user of userData; track user.id) {
+    <app-user-info [user]="user" />
+  } `,
 })
 export class AppComponent {
-  constructor() {}
+  // inject UserService using Angular's dependency injection 
+  // using inject function 
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  async ngOnInit() {
+    this.userData = await this.userService.getUserData();
+  };
+
+  constructor() {
+    // note in constructor we cant use async await syntax
+    // because constructor cannot be async
+    // so we use .then() syntax to handle the promise
+    // as an alternative we can use ngOnInit lifecycle hook
+    // this.userService.getUserData().then((data) => {
+    //   this.userData = data;
+    // });
+  }
 }
--- projects/11-dependency-injection/src/app/user.service.ts
+++ projects/11-dependency-injection/src/app/user.service.ts
@@ -1,5 +1,14 @@
+import { Injectable } from '@angular/core';
 import { data, User } from './data';
 
+// Injectable decorator marks a class as available to be provided and injected as a dependency.
+@Injectable({
+  // providedIn specifies the provider scope of the service.
+  // by setting it to 'root', Angular creates a single, shared instance of UserService and injects it into any class that asks for it.
+  // note that this is singleton scope so entire application will have only one instance of UserService.
+  // Angular does support multiple instances of a service but that is a more advanced topic.
+  providedIn: 'root',
+})
 export class UserService {
   private userData: User[] = data;
 
